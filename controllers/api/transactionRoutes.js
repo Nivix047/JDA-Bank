a007a3389fd5a45e0edd6dcb8a7d527158ca8fcd
Daniel Xu <xudh47@gmail.com>
2022-10-17 19:25:06 -0700

    Updated transaction js

--- controllers/api/transactionRoutes.js
+++ controllers/api/transactionRoutes.js
@@ -63,7 +63,7 @@ router.put("/:username", withAuth, async (req, res) => {
   }
 });
 
-// Transaction front-end js can include a post to this route with all the info waiting: put put post // need to write the js tommorrow
+// debit
 router.post("/", withAuth, async (req, res) => {
   try {
     const newTransaction = await Transaction.create({
@@ -73,6 +73,35 @@ router.post("/", withAuth, async (req, res) => {
     res.status(200).json(newTransaction);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+// credit
+router.post("/:username", withAuth, async (req, res) => {
+  try {
+    const recipientData = await User.findOne({
+      where: {
+        username: req.params.username,
+      },
+    });
+
+    const recipient = recipientData.get({ plain: true });
+
+    const user_id = recipient.id;
+
+    const transaction = await Transaction.create({
+      ...req.body,
+      user_id: user_id,
+    });
+
+    if (!transaction) {
+      res.status(404).json({ message: "No Account found with this id!" });
+      return;
+    }
+
+    res.status(200).json(transaction);
+  } catch (err) {
+    res.status(400).json(err.message);
   }
 });
 
