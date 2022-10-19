a49db234c8e30854118a14c57b7a5d6f9d7ba66d
Daniel Xu <xudh47@gmail.com>
2022-10-18 20:12:14 -0700

    Updated charts

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,27 +1,23 @@
-const Chart = require("chart.js")
+// const Chart = require("chart.js");
 
-const ctx = document.getElementById("credit_debit");
-const myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["${Transactions.transaction_date}"],
-    datasets: [
-      {
-        label: "Credit",
-        backgroundColor: "#3E95CD",
-        data: ["${Transactions.credit}"],
-      },
-      {
-        label: "Debit",
-        backgroundColor: "#8E5EA2",
-        data: ["${Transactions.debit}"],
-      },
-    ],
-  },
-  options: {
-    title: {
-      display: true,
-      text: "Credits and Debits",
+const labels = ["January", "February", "March", "April", "May", "June"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45],
     },
-  },
-});
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {},
+};
+
+const myChart = new Chart(document.getElementById("myChart"), config);
